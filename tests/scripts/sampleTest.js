165a36637263e841a6be998974f8c2b463eb4fc2
Hector Davis <hectord@br.ibm.com>
2017-10-24 11:06:19 -0200

    changing sampleTest.js

--- tests/scripts/sampleTest.js
+++ tests/scripts/sampleTest.js
@@ -1,12 +1,11 @@
 module.exports = {
   'Sample functional test' : function (browser) {
     browser
-      .url('http://google.com')
-      .waitForElementVisible('input.gsfi', 60000, true,
-        function(){}, 'Waiting for Search Input to become visible')
-      .setValue('input.gsfi', 'Test')
-      .waitForElementPresent('#search-page', 60000)
+      .url('http://ppm-planometas-stagingtest.mybluemix.net/')
       .pause(5000)
+      .waitForElementVisible('button[id=queroConhecer]', 60000)
+      // .setValue('input.gsfi', 'Test')
+      // .waitForElementPresent('#search-pag', 60000)
       .end();
   }
 };
