0a0824db154c2a606c77e3132213bc8286a50cbf
Hector Davis <hectord@br.ibm.com>
2017-10-24 11:16:55 -0200

    changing sampleTest.js

--- tests/scripts/sampleTest.js
+++ tests/scripts/sampleTest.js
@@ -3,7 +3,7 @@ module.exports = {
     browser
       .url('http://ppm-planometas-stagingtest.mybluemix.net/')
       .pause(3000)
-      .waitForElementVisible('button[id=queroConhecer]', 60000)
+      .waitForElementVisible('#queroConhecer', 60000)
       // .setValue('input.gsfi', 'Test')
       // .waitForElementPresent('#search-pag', 60000)
       .end();
